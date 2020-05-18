3fc85dddccb5abf7d09564b9d79a32e2f6db9721
Sean <sw92410@hotmail.com>
2020-05-18 17:01:53 -0600

    Updates

---
+++ precache-manifest.f692481fb1567908725163ab0492d904.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54fa5cdeb709467b848c5ce4fc24a8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "377787c236233acf9307",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8158524988da96498d47",
+    "url": "/static/css/main.23ff1851.chunk.css"
+  },
+  {
+    "revision": "377787c236233acf9307",
+    "url": "/static/js/2.d2145905.chunk.js"
+  },
+  {
+    "revision": "17e9b1f3220a82c481c91f9991afc1bc",
+    "url": "/static/js/2.d2145905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8158524988da96498d47",
+    "url": "/static/js/main.d9686372.chunk.js"
+  },
+  {
+    "revision": "560cad56ee1b6d55c234",
+    "url": "/static/js/runtime-main.6b17f562.js"
+  }
+]);
--- src/blog/Blog.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import BlogView from './BlogView';
-
-function Blog() {
-  return <BlogView />;
-}
-
-export default Blog;
