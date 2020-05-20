de8aa78cda1d74fc8034d7d86a402b95990452dc
Sean <sw92410@hotmail.com>
2020-05-19 23:26:58 -0600

    Update HomeView.jsx

--- src/root/HomeView.jsx
+++ src/root/HomeView.jsx
@@ -11,14 +11,16 @@ function HomeView() {
           <h1>Welcome!</h1>
           <p className="fs-18">
             This is my personal website, it hosts my personal projects and progress. You can see
-            them by clicking the links in the navigation bar over on the top. The website is created
-            in React, you can check out the source code{' '}
+            them by clicking the links in the navigation bar over on the top. The website is using
+            React for its front-end, Firebase for its database, and it is hosting as a GitHub user
+            page, you can check out the source code{' '}
             <a
               href="https://github.com/ShouShou92410/ShouShou92410.github.io/tree/Source-Code"
               target="_blank">
               Here
             </a>
-            .
+            . In the future, I want to create a back-end server and have the front-end use the APIs
+            to fetch data.
           </p>
         </Col>
       </Row>
