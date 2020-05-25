9a18b972f5837f01fdc70fe07bf315c978e74a86
Sean <sw92410@hotmail.com>
2020-05-24 21:39:51 -0600

    check user in management page

--- src/management/Management.js
+++ src/management/Management.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../utility/Context';
 import ManagementView from './ManagementView';
 
 function Management() {
+  const { currentUser } = useContext(UserContext);
   const tableList = ['user', 'vocabulary'];
 
-  return <ManagementView tableList={tableList} />;
+  return currentUser && <ManagementView tableList={tableList} />;
 }
 
 export default Management;
