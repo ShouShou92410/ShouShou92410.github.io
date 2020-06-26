2eb86d995e6bc1d0f8ea5924930c5aae4ce7cd0f
Sean <sw92410@hotmail.com>
2020-06-25 18:15:24 -0600

    Add part of speech definition to QuizOngoing side panel

--- src/quiz/Quiz.js
+++ src/quiz/Quiz.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { QuizEnumeration } from '../utility/Enumeration';
 import QuizInitialization from './initialization/QuizInitialization';
 import QuizQuickOngoing from './ongoing/QuizQuickOngoing';
+import QuizOngoingSidePanelView from './ongoing/QuizOngoingSidePanelView';
 import QuizSurvivalOngoing from './ongoing/QuizSurvivalOngoing';
 import QuizFinish from './finish/QuizFinish';
 import Error from '../utility/Error';
@@ -17,56 +18,70 @@ function Quiz() {
   });
   const [quizResults, setQuizResults] = useState([]);
 
-  const renderView = () => {
+  const [quizContentElement, setQuizContentElement] = useState();
+  const [quizDescriptionElement, setQuizDescriptionElement] = useState();
+
+  useEffect(() => {
     switch (quizState) {
       case QuizEnumeration.State.INITIALIZATION:
-        return (
+        setQuizContentElement(
           <QuizInitialization
             quizSetting={quizSetting}
             setQuizSetting={setQuizSetting}
             setQuizState={setQuizState}
           />
         );
+        setQuizDescriptionElement();
+        break;
       case QuizEnumeration.State.ONGOING:
         switch (quizSetting.mode) {
           case QuizEnumeration.Mode.QUICK:
-            return (
+            setQuizContentElement(
               <QuizQuickOngoing
                 quizSetting={quizSetting}
                 setQuizResults={setQuizResults}
                 setQuizState={setQuizState}
               />
             );
+            break;
           case QuizEnumeration.Mode.SURVIVAL:
-            return (
+            setQuizContentElement(
               <QuizSurvivalOngoing
                 quizSetting={quizSetting}
                 setQuizResults={setQuizResults}
                 setQuizState={setQuizState}
               />
             );
+            break;
           default:
-            return <Error />;
+            setQuizContentElement(<Error />);
+            break;
         }
+        setQuizDescriptionElement(<QuizOngoingSidePanelView />);
+        break;
       case QuizEnumeration.State.FINISH:
-        return <QuizFinish quizResults={quizResults} setQuizState={setQuizState} />;
+        setQuizContentElement(<QuizFinish quizResults={quizResults} setQuizState={setQuizState} />);
+        setQuizDescriptionElement();
+        break;
       default:
-        return (
+        setQuizContentElement(
           <QuizInitialization
             quizSetting={quizSetting}
             setQuizSetting={setQuizSetting}
             setQuizState={setQuizState}
           />
         );
+        setQuizDescriptionElement();
+        break;
     }
-  };
+  }, [quizState, quizSetting, quizResults]);
 
   return (
     <>
       <Row>
         <Col lg={2} />
-        <Col lg={7}>{renderView()}</Col>
-        <Col lg={2}>{/* <h4>description</h4> */}</Col>
+        <Col lg={7}>{quizContentElement}</Col>
+        <Col lg={2}>{quizDescriptionElement}</Col>
       </Row>
     </>
   );
---
+++ src/quiz/ongoing/QuizOngoingSidePanelView.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function QuizOngoingSidePanelView() {
+  return (
+    <>
+      <dl>
+        <dt>Noun</dt>
+        <dd className="ml-4">Person, place, or things</dd>
+        <dt>Verb</dt>
+        <dd className="ml-4">Action, state, or occurrence</dd>
+        <dt>Adjective (adj)</dt>
+        <dd className="ml-4">Modifies or describes a noun</dd>
+        <dt>Adverb (adv)</dt>
+        <dd className="ml-4">Modifies or describes a verb, an adjective, or adverb</dd>
+      </dl>
+    </>
+  );
+}
+
+export default QuizOngoingSidePanelView;
