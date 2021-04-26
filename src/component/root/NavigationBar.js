ecbb1207dd287d7679202b0fa05c4653954c51a0
Sean Wen <sw92410@hotmail.com>
2021-04-26 17:22:43 -0600

    - Nav redesign

--- src/component/root/NavigationBar.js
+++ src/component/root/NavigationBar.js
@@ -1,32 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
 
 function NavigationBar() {
-  const [show, setShow] = useState(false);
-  const handleToggleClick = () => setShow(!show);
-  const handleLinkClick = () => setShow(false);
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top" className="navigation" expanded={show}>
-      <Container>
-        <Navbar.Brand as={Link} to="/" onClick={handleLinkClick}>
+    <Nav className="justify-content-center">
+      <Nav.Item>
+        <Nav.Link as={Link} to="/">
           Home
-        </Navbar.Brand>
-        <Navbar.Toggle onClick={handleToggleClick} />
-        <Navbar.Collapse>
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/about" onClick={handleLinkClick}>
-              About
-            </Nav.Link>
-            <Nav.Link as={Link} to="/project" onClick={handleLinkClick}>
-              Project
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link as={Link} to="/about">
+          About
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link as={Link} to="/project">
+          Project
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
   );
 }
 
