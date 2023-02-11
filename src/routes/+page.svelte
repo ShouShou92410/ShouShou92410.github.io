4b57f6b4b5f35f60e46c3a0495fbf732b1521adb
Sean Wen <sw92410@hotmail.com>
2023-02-11 16:52:26 -0700

    Remove unused code and files - Remove Counter.svelte and Header.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import Home from './Home.svelte';
 	import About from './About.svelte';
 	import Education from './Education.svelte';
@@ -13,27 +10,9 @@
 
 <svelte:head>
 	<title>Sean Wen</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Sean Wen's personal website." />
 </svelte:head>
 
-<!-- <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section> -->
 <Home />
 <About />
 <Education />
