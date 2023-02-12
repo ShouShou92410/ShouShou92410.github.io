f7ccc246e3f8e2a9fb74c4a9898382bb47c55343
Sean Wen <sw92410@hotmail.com>
2023-02-12 13:48:17 -0700

    Testing github hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -8,7 +10,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		paths: {
+			base: dev ? '' : '/ShouShou92410.github.io'
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal'
+		// adapter: adapter()
 	}
 };
 
