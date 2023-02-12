aa59e824462c75ad6e414fecab330e24527e0595
Sean Wen <sw92410@hotmail.com>
2023-02-11 17:46:57 -0700

    Responsive text style - Adjust Home.svelte style

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -3,12 +3,20 @@
 </script>
 
 <ContentSection id="">
-	<p>Hello, my name is</p>
-	<h1>Sean Wen</h1>
 	<p>
-		I'm a software developer based in Calgary, Alberta, Canada. <br />
-		I specialized in developing web applications using cutting-edge technologies. <br />
+		👋Hello, my name is
+		<span class="2xl:text-6xl xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl font-semibold"
+			>Sean Wen</span
+		>.
+	</p>
+	<br />
+	<p>
+		I'm a software developer based in Calgary, Alberta, Canada.<br />
+		I specialized in developing web applications using cutting-edge technologies.
+	</p>
+	<br />
+	<p>
 		Currently working as an Associate Software Developer in
-		<a href="https://www.detechtion.com">Detechtion Technologies</a>
+		<a href="https://www.detechtion.com">Detechtion Technologies</a>.
 	</p>
 </ContentSection>
