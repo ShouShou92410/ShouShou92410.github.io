db2c19ed2a922665001e0701f1aff4788fff55cc
Sean Wen <sw92410@hotmail.com>
2023-02-12 14:09:14 -0700

    Testing github page

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -10,13 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		paths: {
-			base: dev ? '' : '/ShouShou92410.github.io'
-		},
-		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-		// For example, instead of '_app', use 'app_', 'internal', etc.
-		appDir: 'internal'
-		// adapter: adapter()
+		adapter: adapter()
 	}
 };
 
