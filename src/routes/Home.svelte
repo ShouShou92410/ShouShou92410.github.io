a8fd95ab2ef8c0fce46e78a3e84e14b86dcf52ff
Sean Wen <sw92410@hotmail.com>
2023-02-12 13:03:11 -0700

    About section - Add about content - Add profile picture

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -5,13 +5,15 @@
 <ContentSection id="">
 	<p>
 		👋Hello, my name is
-		<span class="2xl:text-6xl xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl font-semibold"
+		<span
+			class="2xl:text-6xl xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl font-semibold text-purple-300"
 			>Sean Wen</span
 		>.
 	</p>
 	<br />
 	<p>
-		I'm a software developer based in Calgary, Alberta, Canada.<br />
+		I'm a software developer based in Calgary, Alberta, Canada.
+		<br />
 		I specialized in developing web applications using cutting-edge technologies.
 	</p>
 	<br />
