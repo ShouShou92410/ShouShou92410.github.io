a600807ade6db1ca4a0443b7a56b6e1f431910e5
Sean Wen <sw92410@hotmail.com>
2023-02-13 00:21:34 -0700

    Add SocialIcon.svelte - Extract social icon link to separate component

---
+++ src/lib/components/SocialIcon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let href: string;
+	export let src: string;
+	export let label: string;
+</script>
+
+<a {href} class="flex items-center">
+	<img {src} alt={label} class="w-8 duration-300" />
+	<span class="font-semibold text-lg overflow-hidden pl-3 duration-300 w-0">{label}</span>
+</a>
+
+<style lang="postcss">
+	a:hover > img {
+		@apply scale-125;
+	}
+	a:hover > span {
+		@apply delay-75 duration-500 w-full;
+	}
+</style>
--- src/routes/SocialLinks.svelte
+++ src/routes/SocialLinks.svelte
@@ -3,32 +3,12 @@
 	import linkedin from '$lib/images/linkedin.svg';
 	import email from '$lib/images/email.svg';
 	import resume from '$lib/images/resume.svg';
+	import SocialIcon from '$lib/components/SocialIcon.svelte';
 </script>
 
 <div class="fixed flex flex-col top-1/3 left-10 gap-3">
-	<a href="https://github.com/ShouShou92410" class="flex items-center">
-		<img src={github} alt="GitHub" class="w-8 duration-300" />
-		<span class="font-semibold text-lg overflow-hidden pl-3 duration-300 w-0">GitHub</span>
-	</a>
-	<a href="https://www.linkedin.com/in/yuhsiangwen/" class="flex items-center">
-		<img src={linkedin} alt="LinkedIn" class="w-8 duration-300" />
-		<span class="font-semibold text-lg overflow-hidden pl-3 duration-300 w-0">LinkedIn</span>
-	</a>
-	<a href="mailto:sw92410@hotmail.com" class="flex items-center">
-		<img src={email} alt="email" class="w-8 duration-300" />
-		<span class="font-semibold text-lg overflow-hidden pl-3 duration-300 w-0">Email</span>
-	</a>
-	<a href={resume} class="flex items-center">
-		<img src={resume} alt="resume" class="w-8 duration-300" />
-		<span class="font-semibold text-lg overflow-hidden pl-3 duration-300 w-0">Resume</span>
-	</a>
+	<SocialIcon href="https://github.com/ShouShou92410" src={github} label="GitHub" />
+	<SocialIcon href="https://www.linkedin.com/in/yuhsiangwen" src={linkedin} label="LinkedIn" />
+	<SocialIcon href="mailto:sw92410@hotmail.com" src={email} label="Email" />
+	<SocialIcon href={resume} src={resume} label="Resume" />
 </div>
-
-<style lang="postcss">
-	a:hover > img {
-		@apply scale-125;
-	}
-	a:hover > span {
-		@apply delay-75 duration-500 w-full;
-	}
-</style>
