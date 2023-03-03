40507c276c8065aca90f8e643c0dde3406eb4276
Sean Wen <sw92410@hotmail.com>
2023-03-03 01:15:28 -0700

    build to /docs - Rebuild into /docs - Remove unused images

--- docs/_app/immutable/chunks/0-b5f30397.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-6335b317.js";export{t as component};
--- docs/_app/immutable/chunks/1-97394eec.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-054b337e.js";export{t as component};
