f7dafade4dae662e6e97f615bcc9c291800669e4
Sean Wen <sw92410@hotmail.com>
2023-03-03 21:49:30 -0700

    Fix SocialIcon hover style

--- docs/_app/immutable/chunks/0.000b4208.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_layout.svelte.665c9576.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1.cfa1102d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.688657a7.js";export{t as component};
